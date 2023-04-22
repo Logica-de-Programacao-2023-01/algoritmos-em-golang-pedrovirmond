60947b5ac0c9c528c03b319c0b8362f62fedf997
pedrovirmond <pedrovirmond@gmail.com>
2023-04-22 02:03:02 -0300

    Algoritmos em GO

--- golang.go
+++ golang.go
@@ -1,1 +1,2 @@
-//Faça um algoritmo que leia o salário de um funcionário e calcule o seu novo salário com um aumento de 10% se o salário for menor que R$ 1000,00; ou de 5% se o salário for maior ou igual a R$ 1000,00.
+//Faça um algoritmo que imprima os números de 1 a 100, substituindo os múltiplos de 3 pela palavra "Fizz" e os múltiplos de 5 pela palavra "Buzz". Para os números que são múltiplos de ambos, utilize a palavra "FizzBuzz".
+package main

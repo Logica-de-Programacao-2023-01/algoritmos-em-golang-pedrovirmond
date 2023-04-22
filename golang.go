58b4f2b159fd685c5bcfe18b13ed9591cd246b8c
pedrovirmond <pedrovirmond@gmail.com>
2023-04-22 18:31:46 -0300

    Algoritmos em GO

--- golang.go
+++
@@ -1,2 +0,0 @@
-//Faça um algoritmo que imprima os números de 1 a 100, substituindo os múltiplos de 3 pela palavra "Fizz" e os múltiplos de 5 pela palavra "Buzz". Para os números que são múltiplos de ambos, utilize a palavra "FizzBuzz".
-package main
